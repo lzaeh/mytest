57c14e210fb700e95a614b87ac0a20269d3fb9ee
huye.1104 <huye.1104@bytedance.com>
2023-04-23 19:35:54 +0800

    修改wasm的functionhandler

--- pkg/executor/executortype/wasm/wasmmgr.go
+++ pkg/executor/executortype/wasm/wasmmgr.go
@@ -120,7 +120,7 @@ func MakeWasm(
 	wasm.svcLister = svcInformer.Lister()
 	wasm.svcListerSynced = svcInformer.Informer().HasSynced
 
-	funcInformer.Informer().AddEventHandler(wasm.FuncInformerHandler(ctx))
+	// funcInformer.Informer().AddEventHandler(wasm.FuncInformerHandler(ctx))
 	return wasm, nil
 }
 
